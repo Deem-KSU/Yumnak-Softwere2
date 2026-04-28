174a6a997f42b94d7c7a0dc585fcc7ba01e57f3b
famub <fajralbagmi@gmail.com>
2026-04-28 10:13:09 +0300

    Create db_connection.php
    thnx salam

---
+++ db_connection.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root"; 
+$dbname = "yumnak";
+$port = 8889; 
+
+$conn = mysqli_connect($servername, $username, $password, $dbname, $port);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
